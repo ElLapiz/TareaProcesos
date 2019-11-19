9e74461120decf3a2b988a4c74549e2293e0207d
bernalvm <bernal-r>
2019-11-19 12:03:28 -0600

    Separate minners files

---
+++ incl/memory/Collect_Memory_Minner.cpp
@@ -0,0 +1,9 @@
+#include "Collect_Memory_Minner.h"
+#include "../../../../../../usr/include/stdio.h"
+
+void collectMemData();
+
+void collectMemData() {
+    printf("Hola vengo a recolectar datos de memoria\n");
+    //sleep(1);
+}
